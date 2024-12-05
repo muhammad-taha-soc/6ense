1d339a1bb0cfeeecaca888cc07f3171ea6ce9b57
Muhammad Taha <Muhammad.Taha@soc-solution.com>
2024-12-05 19:18:46 +0500

    feat: add hero section

---
+++ src/components/containers/LinkButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function LinkButton({ text, variant }) {
+  return (
+      <button
+          className={`${variant == "light" ? "bg-white text-black":"bg-black text-white"} font-inter py-2 px-5 rounded-full transition-colors duration-300 text-base font-semibold whitespace-nowrap md:text-base`}
+      >
+          <a href="#" className='flex flex-row justify-between items-center font-inter text-base font-semibold' onClick='' >
+              {text}
+              <img
+                  src={`/assets/img/link-arrow-icon-${variant == "light"?"dark":"light"}.svg`}
+                  alt='link'
+                  className='ml-2 '
+                  // onLoad={handleImageLoad} // Trigger image load handler
+                  loading="lazy"
+              />
+          </a>
+      </button>
+  )
+}
