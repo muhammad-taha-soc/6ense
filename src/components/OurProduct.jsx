d10f3a0d436863c2c8718a201e7d2674931c6b99
muhammad taha <muhammad.taha@soc-solution.com>
2024-12-08 18:48:08 +0500

    feat: update changes

--- src/components/OurProduct.jsx
+++ src/components/OurProduct.jsx
@@ -12,12 +12,12 @@ export default function OurProduct() {
                     First contact with 6ENSE. We are trasformin everithing into one secure digital system made from people.
                 </h1>
                 <div className='flex flex-col md:flex-row items-center justify-between w-full my-8'>
-                    {[{ title: 'Totaly Supply', text: '9,63 Mld' }, { title: 'Totaly Supply', text: '9,63 Mld' }, { title: 'Market cap', text: '5 Mln' }].map((data, index) => (
+                    {[{ title: 'Totaly Supply', text: '9,63 Mld' }, { title: 'Market cap', text: '5 Mln' }].map((data, index) => (
                         <Card title={data.title} text={data.text} variant="light" />
                     ))}
-                    {/* <div className='bg-[#F1F1F1] font-inter mr-6 rounded-2xl w-full px-6 py-6'>
-                        <img src="/assets/logo/logo.svg" alt="Logo" className="h-8 w-auto" />
-                    </div> */}
+                    <div className='border-[#F1F1F1] border font-inter text-center mr-auto md:mr-6 mb-2 rounded-2xl w-full px-6 py-6`'>
+                        <img src="/assets/img/product-section.png" alt="product-section" className="mx-auto px-6 py-6" />
+                    </div>
                 </div>
                 <p className="lg:text-base  my-6 font-inter font-normal">
                     6ense | 6OS Tokens are ERC-20 utility token fuelling thewhole Plantogroup 6ENSE Ecosystem.<br />Allows users to receive a discount on our platform of real economics of products and as well as allowing them to have priority access to DEX&Dapp implementation with LOWEST commission.                  </p>
